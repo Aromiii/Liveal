db5e1ff30984a5f8213526adf1d30b61de90d469
Aaro <aaro.heroja@gmail.com>
2023-02-10 20:32:36 +0200

    better spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,14 +35,23 @@ const Home: NextPage = () => {
           </svg>
         </nav>
         <div className="flex mt-5 gap-5">
-          <aside className="w-1/3 bg-white rounded-lg">
+          <aside className="w-1/6">
+            <div className="h-36 bg-white rounded-lg">
 
+            </div>
+            <div className="bg-white rounded-lg h-72 mt-5">
+              <h1>
+                Friends
+              </h1>
+            </div>
           </aside>
-          <main className="w-full bg-white h-96 rounded-lg">
+          <main className=" bg-white h-96 w-1/2 mx-auto rounded-lg">
 
           </main>
-          <aside className="w-1/3 bg-white rounded-lg">
-
+          <aside className="w-1/6 bg-white rounded-lg">
+            <h1>
+              Chats
+            </h1>
           </aside>
         </div>
       </div>
