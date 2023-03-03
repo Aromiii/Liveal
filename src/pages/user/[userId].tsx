8bfc3ceadf3a8fb829db6dbd1c7442d4f7a0651a
Aaro <aaro.heroja@gmail.com>
2023-03-03 23:00:27 +0200

    feat: adapted ui to mobile

--- src/pages/user/[userId].tsx
+++ src/pages/user/[userId].tsx
@@ -3,8 +3,9 @@ import { useSession } from "next-auth/react";
 import FriendsNav from "../../components/navs/friendsNav";
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { prisma } from "../../server/db";
+import Link from "next/link";
 
-const Profile = ({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const User = ({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { status } = useSession();
 
   if (status == "authenticated") {
@@ -13,13 +14,16 @@ const Profile = ({ user }: InferGetServerSidePropsType<typeof getServerSideProps
       <div className="pt-5 relative">
         <img className="h-[30vh] object-cover w-full rounded-lg"
              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.forestryengland.uk%2Fsites%2Fdefault%2Ffiles%2Fmedia%2FSavernake.jpg&f=1&nofb=1&ipt=bdec4b84b78ef7739a8e5d24a31ffe772381fae3ac10d3f2b9dfcd4068340f18&ipo=images" />
-        <div className="flex p-4 absolute w-full top-[60%]">
-          <div className="w-1/6 md:block hidden">
+        <div className="md:flex p-4 absolute w-full top-[60%]">
+          <div className="md:w-1/6 w-1/2">
             <div className="h-36 bg-white rounded-lg flex place-items-center flex-col">
               <img className="w-24 h-24 rounded-full object-cover m-2" alt="Profile picture"
                    src={user.image} />
               <h1 className="font-bold text-lg">{user.name}</h1>
             </div>
+            <Link href={`/user/${user.id}/friends`} className="md:hidden block p-1 px-5 text-2xl mt-2 rounded-lg bg-white text-center">
+              Friends
+            </Link>
             <button className="bg-red-500 text-white p-2 px-5 text-2xl w-full mt-4 rounded-lg">
               Start chat
             </button>
@@ -30,8 +34,8 @@ const Profile = ({ user }: InferGetServerSidePropsType<typeof getServerSideProps
 
             </main>
           </div>
-          <div className="w-1/6 md:block hidden">
-            <FriendsNav/>
+          <div className="md:w-1/6 w-1/3 bg-red-500 h-96 md:block hidden">
+            <FriendsNav />
           </div>
         </div>
       </div>
@@ -41,18 +45,19 @@ const Profile = ({ user }: InferGetServerSidePropsType<typeof getServerSideProps
   return null;
 };
 
-export default Profile;
+export default User;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const user = await prisma.user.findUnique({
     where: {
       id: context.query.userId
     }
-  })
+  });
 
   return {
     props: {
       user: {
+        id: user.id,
         name: user.name,
         image: user.image
       }
