084c5bbddb1f42d614b33b16ecf1db4fef0b9efe
Aromi <93449713+Aromiii@users.noreply.github.com>
2023-03-04 22:31:00 +0200

    feat: made mobile ui even better

--- src/pages/user/[userId].tsx
+++ src/pages/user/[userId].tsx
@@ -16,20 +16,19 @@ const User = ({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) 
              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.forestryengland.uk%2Fsites%2Fdefault%2Ffiles%2Fmedia%2FSavernake.jpg&f=1&nofb=1&ipt=bdec4b84b78ef7739a8e5d24a31ffe772381fae3ac10d3f2b9dfcd4068340f18&ipo=images" />
         <div className="md:flex p-4 absolute w-full top-[60%]">
           <div className="flex md:block w-full md:w-1/6">
-            <div className="w-2/5 md:w-full mr-auto">
-              <div className="h-36 bg-white rounded-lg flex place-items-center flex-col">
-                <img className="w-24 h-24 rounded-full object-cover m-2" alt="Profile picture"
-                     src={user.image} />
+            <div className="w-full mr-auto md:block flex gap-5">
+              <div className="w-3/5 md:w-full h-36 bg-white rounded-lg flex place-items-center flex-col">
+                <img className="w-24 h-24 rounded-full object-cover m-2" alt="Profile picture" src={user.image} />
                 <h1 className="font-bold text-lg">{user.name}</h1>
               </div>
-              <Link href={`/user/${user.id}/friends`} className="md:hidden block p-1 px-5 text-2xl mt-2 rounded-lg bg-white text-center">
-                Friends
-              </Link>
-              <button className="bg-red-500 text-white p-2 px-5 text-2xl w-full mt-4 rounded-lg">
-                Start chat
-              </button>
-            </div>
-            <div className="md:hidden block w-1/2 bg-white rounded-lg">
+              <div className="w-2/5 md:w-full flex-col flex">
+                <button className="bg-red-500 text-white p-2 px-5 text-2xl w-full md:mt-4 rounded-lg">
+                  Start chat
+                </button>
+                <Link href={`/user/${user.id}/friends`} className="md:hidden h-full mt-4 block p-1 px-5 md:mt-2 rounded-lg bg-white flex place-items-center place-content-center">
+                  Friends
+                </Link>
+              </div>
             </div>
           </div>
           <div className="mx-auto md:w-1/2 w-full h-72">
