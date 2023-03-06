dd81b13c1be97fdda78acc4daed1d7103f27594d
Aaro <aaro.heroja@gmail.com>
2023-03-06 16:45:05 +0200

    feat: api boilerplate and data checks

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -12,7 +12,21 @@ const handler = async (req: NextRequest, res: NextResponse) => {
   }
 
   if (req.method == "POST") {
+    const schema = z.object({
+      displayName: z.string().min(3).max(50),
+      username: z.string().min(3).max(50),
+      description: z.string().max(1000).optional()
+    });
 
+    try {
+      const body = schema.parse(JSON.parse(req.body));
+      res.status(200).json({ message: "200" });
+      return;
+
+    } catch (error) {
+      res.status(400).json({ message: "Values you provided are invalid" });
+      return;
+    }
   }
 
 };
