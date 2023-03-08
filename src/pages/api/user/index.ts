74ca953232bb93877cc1450113a40a9635357f1c
Aaro <aaro.heroja@gmail.com>
2023-03-08 19:00:51 +0200

    feat: profile making form working

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -2,6 +2,7 @@ import type { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../../server/auth";
 import { z } from "zod";
+import { prisma } from "../../../server/db";
 
 const handler = async (req: NextRequest, res: NextResponse) => {
   const session = await getServerSession(req, res, authOptions);
@@ -17,13 +18,44 @@ const handler = async (req: NextRequest, res: NextResponse) => {
       description: z.string().max(1000).optional()
     });
 
-    const body = schema.safeParse(JSON.parse(req.body))
+    const body = schema.safeParse(JSON.parse(req.body));
 
     if (!body.success) {
-      res.status(400).json({ message: "Data you provided is not in correct format"})
+      res.status(400).json({ message: "Data you provided is not in correct format" });
+      return;
     }
-  }
 
+    const isUsernameTaken = await prisma.user.findFirst({
+      where: {
+        username: body.data.username
+      }
+    });
+
+    if (isUsernameTaken) {
+      res.status(400).json({ message: "Username is taken" });
+      return;
+    }
+
+    try {
+      await prisma.user.update({
+        where: {
+          id: session.user.id
+        },
+        data: {
+          name: body.data.displayName,
+          username: body.data.username,
+          description: body.data.description,
+          profileCreated: true
+        }
+      });
+    } catch (error) {
+      res.status(400).json({ message: "Error occurred" });
+      return;
+    }
+
+    res.status(200).json({ message: "Profile created" });
+    return;
+  }
 };
 
 export default handler;
