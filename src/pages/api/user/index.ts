cff6fae8ca271ed87f66b10612340fd5f5b5f2e8
Aaro <aaro.heroja@gmail.com>
2023-03-13 21:27:17 +0200

    feat: profile editing page

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -11,20 +11,31 @@ const handler = async (req: NextRequest, res: NextResponse) => {
     res.status(401).json({ message: "You must be logged in" });
   }
 
-  if (req.method == "POST") {
-    const schema = z.object({
-      displayName: z.string().min(3).max(50),
-      username: z.string().min(3).max(50),
-      description: z.string().max(1000).optional()
-    });
+  const schema = z.object({
+    displayName: z.string().min(3).max(50),
+    username: z.string().min(3).max(50),
+    description: z.string().max(1000).optional()
+  });
 
-    const body = schema.safeParse(JSON.parse(req.body));
+  const body = schema.safeParse(JSON.parse(req.body));
 
-    if (!body.success) {
-      res.status(400).json({ message: "Data you provided is not in correct format" });
-      return;
+  if (!body.success) {
+    res.status(400).json({ message: "Data you provided is not in correct format" });
+    return;
+  }
+
+  const isUsernameTaken = await prisma.user.findFirst({
+    where: {
+      username: body.data.username
     }
+  });
 
+  if (isUsernameTaken) {
+    res.status(400).json({ message: "Username is taken" });
+    return;
+  }
+
+  if (req.method == "POST") {
     const isProfileCreated = await prisma.user.findFirst({
       where: {
         id: session.user.id
@@ -34,17 +45,6 @@ const handler = async (req: NextRequest, res: NextResponse) => {
     if (isProfileCreated.profileCreated) {
       res.status(409).json({ message: "Profile is already created" })
       return
-    }
-
-    const isUsernameTaken = await prisma.user.findFirst({
-      where: {
-        username: body.data.username
-      }
-    });
-
-    if (isUsernameTaken) {
-      res.status(400).json({ message: "Username is taken" });
-      return;
     }
 
     try {
@@ -65,6 +65,27 @@ const handler = async (req: NextRequest, res: NextResponse) => {
     }
 
     res.status(200).json({ message: "Profile created" });
+    return;
+  }
+
+  if (req.method == "PUT") {
+    try {
+      await prisma.user.update({
+        where: {
+          id: session.user.id
+        },
+        data: {
+          name: body.data.displayName,
+          username: body.data.username,
+          description: body.data.description
+        }
+      })
+    } catch (error) {
+      res.status(400).json({ message: "Error occurred" });
+      return;
+    };
+
+    res.status(200).json({ message: "Profile updated" });
     return;
   }
 };
--- src/pages/user/edit.tsx
+++ src/pages/user/edit.tsx
@@ -1,12 +1,56 @@
-import { type GetServerSidePropsContext, type NextPage } from "next";
+import type { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../server/auth";
+import { prisma } from "../../server/db";
+import BgWithLivealLogo from "../../components/bgWithLivealLogo";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-const Edit: NextPage = () => {
+const Edit: NextPage = ({ user }: InferGetServerSidePropsType<GetServerSideProps>) => {
+  const router = useRouter()
+  const { data: session } = useSession()
+  const [displayName, setDisplayName] = useState(user.name);
+  const [username, setUsername] = useState(user.username);
+  const [desc, setDesc] = useState(user.description);
+
+  const updateAccount = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("/api/user", {
+      method: "PUT",
+      credentials: "include",
+      body: JSON.stringify({
+        displayName: displayName,
+        username: username,
+        description: desc
+      })
+    });
+    const body = await response.json();
+    alert(body.message);
+
+    if (response.status < 299) {
+      await router.replace("/")
+    }
+  };
+
   return <>
-    <div>
-
-    </div>
+    <BgWithLivealLogo>
+      <h1 className="text-2xl text-center mb-5">Create your account</h1>
+      <form className="flex flex-col gap-4" onSubmit={updateAccount}>
+        <div className="w-full gap-4 flex">
+          <input minLength={3} maxLength={50} required className="bg-gray-200 rounded-lg w-full p-2"
+                 placeholder=" Display name..." onChange={event => setDisplayName(event.target.value)} defaultValue={displayName}/>
+          <input minLength={3} maxLength={50} required className="bg-gray-200 rounded-lg w-full p-2"
+                 placeholder=" Username..." onChange={event => setUsername(event.target.value)} defaultValue={username}/>
+        </div>
+        <textarea rows={3} className="bg-gray-200 rounded-lg w-full p-2" placeholder=" Your profile description..."
+                  maxLength={1000} onChange={event => setDesc(event.target.value)} defaultValue={desc}/>
+        <button className="liveal-button ml-auto">
+          Update profile
+        </button>
+      </form>
+    </BgWithLivealLogo>
   </>
 };
 
@@ -15,9 +59,24 @@ export default Edit;
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getServerSession(context.req, context.res, authOptions)
 
+  const user = await prisma.user.findFirst({
+    where: {
+      id: session.user.id
+    }
+  })
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: true
+      }
+    }
+  }
+
   return {
     props: {
-
+      user: user
     }
   };
 }
