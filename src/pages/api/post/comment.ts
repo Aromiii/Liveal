3b783883006fe32f8aa60ff91999cea91701aac6
Aaro <aaro.heroja@gmail.com>
2023-03-15 21:16:18 +0200

    feat: data validation

--- src/pages/api/post/comment.ts
+++ src/pages/api/post/comment.ts
@@ -1,6 +1,7 @@
 import type { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../../server/auth";
+import { z } from "zod";
 
 const handler = async (req: NextRequest, res: NextResponse) => {
   const session = await getServerSession(req, res, authOptions);
@@ -8,6 +9,20 @@ const handler = async (req: NextRequest, res: NextResponse) => {
   if (!session) {
     res.status(401).json({ message: "You must be logged in" });
     return;
+  }
+
+  const schema = z.object({
+    text: z.string().min(1).max(200)
+  })
+
+  const body = schema.safeParse(JSON.parse(req.body));
+
+  if (!body.success) {
+    res.status(400).json({ message: "Data you provided is not in correct format" });
+    return;
+
+  if (req.method == "POST") {
+
   }
 
 
