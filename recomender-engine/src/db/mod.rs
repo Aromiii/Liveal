70a53a703d776c11e9a04c36f299d4aa31e03e81
Aaro <aaro.heroja@gmail.com>
2023-05-07 21:55:52 +0300

    feat: made get request to auth to get user session

--- recomender-engine/src/db/mod.rs
+++ recomender-engine/src/db/mod.rs
@@ -2,21 +2,11 @@ use sqlx::MySqlPool;
 use crate::db::types::Post;
 
 pub async fn get_posts(pool: &rocket::State<MySqlPool>) -> Vec<Post> {
-    let data = sqlx::query!("SELECT content, id, likes FROM Post")
+    let data: Vec<Post> = sqlx::query_as!(Post, "SELECT content, id, likes FROM Post")
         .fetch_all(pool.inner())
         .await.expect("Unable to query Post table");
 
-    let mut posts: Vec<Post> = vec![];
-
-    for i in data.iter() {
-        posts.push(Post {
-            id: String::from_utf8((*i.id).to_vec()).unwrap(),
-            content: String::from_utf8((*i.content).to_vec()).unwrap(),
-            likes: i.likes,
-        })
-    };
-
-    return posts;
+    return data;
 }
 
 pub mod types;
--- recomender-engine/src/main.rs
+++ recomender-engine/src/main.rs
@@ -1,12 +1,37 @@
 #[macro_use]
 extern crate rocket;
 
-mod rate_post;
+mod engine;
 
+
+use rocket::Request;
 use sqlx::*;
 
 mod db;
 use db::types::Post;
+use crate::auth::check;
+
+mod auth {
+    async fn get_session() {
+        let url = "http://localhost:3000/api/auth/session";
+
+        let client = reqwest::Client::new();
+        let response = client
+            .get(url)
+            .send()
+            .await
+            .expect("failed to get response")
+            .text()
+            .await
+            .expect("failed to get payload");
+
+        println!("{:#?}", response)
+    }
+
+    pub async fn check() {
+        get_session().await;
+    }
+}
 
 #[get("/")]
 async fn index(pool: &rocket::State<MySqlPool>) -> String {
