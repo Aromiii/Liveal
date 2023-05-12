addcdaf14aa01930e6144e9ff44acc71139b05e4
Aaro <aaro.heroja@gmail.com>
2023-05-12 16:57:27 +0300

    feat: prepared sql db to requests for tops posts

--- recomender-engine/src/engine/mod.rs
+++ recomender-engine/src/engine/mod.rs
@@ -1,4 +1,4 @@
-use sqlx::MySqlPool;
+use sqlx::{MySql, MySqlPool, Pool};
 use crate::db;
 use crate::db::types::Post;
 
@@ -6,4 +6,13 @@ pub async fn get_posts(pool: &rocket::State<MySqlPool>, user_id: String) -> Vec<
     let posts = db::get_all_posts(pool).await;
 
     return posts;
+}
+
+// TODO get pool somehow to fetch data from the db
+pub async fn top_posts(pool: Pool<MySql>) -> Vec<Post> {
+    let data: Vec<Post> = sqlx::query_as!(Post, "SELECT content, id, likes, ((SELECT COUNT(*) FROM Comment WHERE postId = Post.id) + likes) / 2 AS average FROM Post ORDER BY average DESC LIMIT 1000;")
+        .fetch_all(pool)
+        .await.expect("Unable to query Post table");
+
+    return data;
 }
