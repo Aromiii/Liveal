aac061b14d43ee44d6dd8ccad7fd71d4b5502e5b
Aaro_laptop <aaro.heroja@gmail.com>
2023-05-23 14:32:05 +0300

    feat: added env variables loading from .env file

--- recomender-engine/src/db/mod.rs
+++ recomender-engine/src/db/mod.rs
@@ -1,8 +1,16 @@
+use std::env;
 use sqlx::{MySql, MySqlPool, Pool};
 
 pub async fn create_pool() -> Pool<MySql> {
-    let database_url = "mysql://127.0.0.1:3306";
-    let pool = MySqlPool::connect(database_url)
+    dotenv::dotenv().ok();
+
+    // Access the environment variable
+    let database_url = match env::var("DATABASE_URL") {
+        Ok(value) => value,
+        Err(e) => e.to_string()
+    };
+
+    let pool = MySqlPool::connect(&*database_url)
         .await
         .expect("Failed to connect to database");
 
