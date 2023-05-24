427afc43af9290a8bbbdebb0cf5245da85e7edb4
Aaro <aaro.heroja@gmail.com>
2023-05-24 21:27:39 +0300

    refactor: removed dead code

--- recomender-engine/src/db/types.rs
+++ recomender-engine/src/db/types.rs
@@ -1,16 +1,4 @@
 use rocket::serde::{Serialize, Deserialize};
-use sqlx::FromRow;
-
-#[derive(Debug, Serialize, Deserialize, Clone, FromRow)]
-pub struct Post {
-    pub(crate) id: String,
-    pub(crate) content: String,
-    pub(crate) likes: i32,
-    pub(crate) username: Option<String>,
-    pub(crate) name: Option<String>,
-    pub(crate) user_image: Option<String>,
-    pub(crate) created_at: Option<String>,
-}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct PostWithRating {
