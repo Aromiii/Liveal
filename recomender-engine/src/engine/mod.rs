f79bdd6a54107496ad183c8ae601d65c740cb866
Aaro <aaro.heroja@gmail.com>
2023-05-26 23:07:18 +0300

    fix: fixed sql qery to db getting personalised posts

--- recomender-engine/src/engine/mod.rs
+++ recomender-engine/src/engine/mod.rs
@@ -17,7 +17,7 @@ pub async fn get_generic_posts(top_posts: Vec<PostWithRating>, page_number: u32)
 }
 
 pub async fn get_customised_posts(pool: &rocket::State<MySqlPool>, top_posts: Vec<PostWithRating>, user_id: &str, page_number: u32) -> Vec<PostWithRating> {
-    let mut posts = sqlx::query_as!(PostWithRating, "SELECT Post.id, Post.content, Post.likes, User.id as user_id, User.username, User.name, User.image AS user_image, DATE_FORMAT(createdAt, '%Y-%m-%d %H:%i:%s') AS created_at, (rating + ((SELECT COUNT(*) FROM Comment WHERE postId = Post.id) * 1.5) + Post.likes) AS rating FROM Post JOIN User ON Post.userId = User.id JOIN Friendship f1 ON f1.user1Id = Post.userId OR f1.user2Id = Post.userId JOIN Friendship f2 ON (f2.user1Id = Post.userId OR f2.user2Id = Post.userId) AND (f2.user1Id = ? OR f2.user2Id = ?) WHERE Post.userId != ? ORDER BY rating DESC LIMIT ? OFFSET ?", user_id, user_id, user_id, PERSONAL_POSTS_SIZE, (page_number - 1) * PERSONAL_POSTS_SIZE)
+    let mut posts = sqlx::query_as!(PostWithRating, "SELECT Post.id, Post.content, Post.likes, User.id as user_id, User.username, User.name, User.image AS user_image, DATE_FORMAT(createdAt, '%Y-%m-%d %H:%i:%s') AS created_at, (rating + ((SELECT COUNT(*) FROM Comment WHERE postId = Post.id) * 1.5) + Post.likes) AS rating FROM Post JOIN User ON Post.userId = User.id JOIN Friendship f1 ON f1.user1Id = Post.userId OR f1.user2Id = Post.userId JOIN Friendship f2 ON (f2.user1Id = Post.userId OR f2.user2Id = Post.userId) AND (f2.user1Id = ? OR f2.user2Id = ?) WHERE Post.userId != ? ORDER BY rating DESC LIMIT ? OFFSET ?", user_id, user_id, user_id, PERSONAL_POSTS_SIZE, page_number * PERSONAL_POSTS_SIZE)
         .fetch_all(pool.inner())
         .await.unwrap();
 
